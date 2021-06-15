ca2f7bf8d25d344ce9c2ddf1e7c41d8456ba312e
Akshay <akhareakshay2@gmail.com>
2021-06-15 19:48:02 +0530

    var variable program

---
+++ vars.ts
@@ -0,0 +1,15 @@
+var myName:string="Akshay";
+
+function setNames(){
+    var myName:string="Akhare";
+    console.log("Inside the function : "+myName);
+}
+setNames();
+console.log("Outside the function : "+myName);
+
+
+var index = 0;
+for(var index = 0; index <=5 ; index++){
+    console.log("Index Value Inside For Loop:: " + index);
+}
+console.log("Inde value Outside For Loop :: " + index);
