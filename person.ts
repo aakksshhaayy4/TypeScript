02168725e917577a069497ca75a69db4c4af23a4
Akshay <akhareakshay2@gmail.com>
2021-06-15 19:43:58 +0530

    values printing on console

---
+++ person.ts
@@ -0,0 +1,6 @@
+var pname:string = "Akshay";
+var age:number = 23;
+var gender:string = "Male";
+var isEmployed:boolean = false;
+
+console.log(`Name = ${pname}, Age = ${age}, Gender = ${gender}, isEmployed = ${isEmployed}`);
